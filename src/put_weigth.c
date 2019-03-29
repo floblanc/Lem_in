e411409f39978caa81a15e329cf5925f48b67232
Florian BLANCHARD <floblanc@e2r11p18.42.fr>
2019-03-29 20:41:15 +0100

    c'est mieux mais c'est toujours la merde

--- src/put_weigth.c
+++ src/put_weigth.c
@@ -6,7 +6,7 @@
 /*   By: floblanc <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/03/29 16:12:20 by floblanc          #+#    #+#             */
-/*   Updated: 2019/03/29 20:11:20 by floblanc         ###   ########.fr       */
+/*   Updated: 2019/03/29 20:40:48 by floblanc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,22 +15,23 @@
 int		all_wth_done(t_room *tab, int **matrix, int size)
 {
 	int	i;
+	int	j;
+	int deg;
 
 	i = 0;
 	while (i < size)
-	{/*
-		si fourmillere coupe en 2 avec start-end (avec ou sans autres salles)d'un cote 
-		et le reste qui est lie le l'autre sans lien en les 2 cote c'est la merde
-		mais si on parse en bloquant quand start est atteint, ca nique d'autre cas
-		dans lesquels si on a 2 chemins distincts avec un beaucoup plus long que
-		l'autre ca stop trop tot et certaines salles du chemin plus long sont mise
-		a -1 alors qu'il serait utile avec beaucoup de fourmis
-
-		---conditions d'arrets a regler ou autre astuce a trouver---
-	*/
-		if (/*(*/matrix[i][i] == 0 /*|| tab[0].wth != 0)*/ && tab[i].wth == 0 && i != 1)
+	{
+		if (matrix[i][i] == 0 && tab[i].wth == 0 && i != 1)
 			tab[i].wth = -1;
-		if (tab[i].wth == 0 && i != 1 && tab[0].wth == 0)
+		j = 0;
+		deg = 0;
+		while (j < size)
+		{
+			if (tab[i].wth == 0 && tab[j].wth == 0 && matrix[i][j] == -1)
+				deg++;;
+			j++;
+		}
+		if (tab[i].wth == 0 && i != 1 && tab[0].wth == 0 && deg != matrix[i][i])
 			return (0);
 		i++;
 	}

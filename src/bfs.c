2856ddf382a3e683497db0ab117d9d5575bee5aa
Florian Blanchard <floblanc@MacBook-Pro-de-Florian.local>
2019-04-03 02:07:41 +0200

    idee de condition d'arret pour le bfs

--- src/bfs.c
+++ src/bfs.c
@@ -6,12 +6,17 @@
 /*   By: floblanc <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/02 12:01:32 by floblanc          #+#    #+#             */
-/*   Updated: 2019/04/02 18:56:58 by floblanc         ###   ########.fr       */
+/*   Updated: 2019/04/03 02:07:05 by floblanc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/lem_in.h"
 
+
+/*
+IDEE CONDITION D'ARRET BFS :
+tant que tous les chemins lies a start (tab[0]) n'ont pas un poids (wth)
+*/
 void		bfs(int **matrix, int i, int j, t_room *tab)
 {
 	int	d;

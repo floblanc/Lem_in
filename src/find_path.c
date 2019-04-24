9e6bf7d0eaa0d198cc5fd6acd149ddfcf96cbdf1
Florian Blanchard <floblanc@MacBook-Pro-de-Florian.local>
2019-04-24 07:39:47 +0200

    tri des paths, pour pouvoir calc_step

--- src/find_path.c
+++ src/find_path.c
@@ -6,11 +6,48 @@
 /*   By: maginist <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/09 13:45:26 by maginist          #+#    #+#             */
-/*   Updated: 2019/04/22 16:27:43 by floblanc         ###   ########.fr       */
+/*   Updated: 2019/04/24 07:37:01 by floblanc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/lem_in.h"
+
+void	swap_path(t_path *new, int size, int i)
+{
+	int	tmp[size];
+	int	save_len;
+	int	j;
+
+	j = 0;
+	while (j < size && new->path[i][j] != 0)
+		tmp[j] = new->path[i][j++];
+	save_len = new->len[i];
+	j = 0;
+	while (j < size && (new->path[i][j] != 0 || new->path[i + 1][j] != 0))
+		new->path[i][j] = new->path[i + 1][j++];
+	new->len[i] = new->len[i + 1];
+	j = 0;
+	while (j < size && new->path[i + 1][j] != 0)
+		new->path[i + 1][j] = tmp[j++];
+	new->len[i + 1] = save_len;
+}
+
+void	sort_paths(t_path *new, int size)
+{
+	int	i;
+
+	i = 0;
+	while (i <= new->path_n)
+	{
+		if ((new->len[i] <= 0 || new->len[i] > new->len[i + 1])
+				&& new->len[i + 1] > 0)
+		{
+			swap_path(new, size, i);
+			i = 0;
+		}
+		i++;
+	}
+}
 
 int		all_len_complete(t_path *new)
 {
@@ -32,7 +69,7 @@ int		analyse_block(t_room *tab, t_path *new, int *way, int pos)
 	int	i;
 
 	i = 0;
-//	printf(" %d\n",new->path[*way][pos]);
+	//	printf(" %d\n",new->path[*way][pos]);
 	new->path[*way][pos] *= -1;
 	block = tab[new->path[*way][pos]].taken - 1;
 	tab[new->path[*way][pos]].taken = *way + 1;
@@ -40,18 +77,18 @@ int		analyse_block(t_room *tab, t_path *new, int *way, int pos)
 		i++;
 	if (new->path[block][i] == -1)
 		new->path[block][i--] = 0;
-//	printf("new->path[%d][%d] = %d != new->path[%d][%d] = %d\n", block, i, new->path[block][i], *way, pos, new->path[*way][pos]);
+	//	printf("new->path[%d][%d] = %d != new->path[%d][%d] = %d\n", block, i, new->path[block][i], *way, pos, new->path[*way][pos]);
 	while (new->path[block][i] != new->path[*way][pos])
 	{
-	//	if (i >= 0)
-	//		printf("new->path[%d][%d] = %d != new->path[%d][%d] = %d\n", block, i, new->path[block][i], *way, pos, new->path[*way][pos]);
+		//	if (i >= 0)
+		//		printf("new->path[%d][%d] = %d != new->path[%d][%d] = %d\n", block, i, new->path[block][i], *way, pos, new->path[*way][pos]);
 		tab[new->path[block][i]].taken = 0;
 		new->path[block][i] = 0;
 		i--;
 	}
-		new->path[block][i] = 0;
-		new->len[block] = 0;
-		*way = block - 1;
+	new->path[block][i] = 0;
+	new->len[block] = 0;
+	*way = block - 1;
 	return (i - 1);
 }
 
@@ -80,15 +117,15 @@ int		way_is_possible(int **matrix, t_room *tab, t_path *new, int way)
 			//		printf("check : (((tab[%d].taken (%d) <= 0 && ((tab[%d].taken (%d) > (way (%d) + 1) * -1 (%d) && !(tab[%d].taken(%d) == -1 && pos(%d) == 0))) || tab[%d].taken (%d) == 0)) || i (%d)== 1) && (best(%d) == 0 || tab[%d].wth(%d) < tab[%d].wth (%d)))\n\n", i, tab[i].taken, i, tab[i].taken, way, (way + 1) * -1, i, tab[i].taken, pos, i, tab[i].taken, i, best, i, tab[i].wth, best, tab[best].wth);
 			if ((!(tab[i].taken) && (best == 0 || tab[i].wth < tab[best].wth)))
 				best = i;
-			else if (pos > 0 && tab[i].taken > 0 && (tab[i].taken >/*si on l'inverse on a d'autres chemins ca peut etre interessant de lancer les 2 et garder le meilleur*/ way + 1
-							&& (best == 0 || tab[block].wth > tab[i].wth)))
+			else if (pos > 0 && tab[i].taken > 0 && (tab[i].taken </*si on l'inverse on a d'autres chemins ca peut etre interessant de lancer les 2 et garder le meilleur*/ way + 1
+						&& (best == 0 || tab[block].wth > tab[i].wth)))
 				block = i;
 		}
-			if (best == 1)
-				break ;
+		if (best == 1)
+			break ;
 		i++;
 	}
-//	printf("best = %d %s.taken = %d pour le way %d\n", best, tab[best].name, tab[best].taken,  way);
+	//	printf("best = %d %s.taken = %d pour le way %d\n", best, tab[best].name, tab[best].taken,  way);
 	if (best != 0 && best != 1)
 		tab[best].taken = way + 1;
 	else if (!(best) && block > 0)
